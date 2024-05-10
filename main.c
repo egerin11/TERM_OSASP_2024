08ce40cf40f64f3e21bb2ce612243c95a25f3072
egerin <zhenya.eww@gmail.com>
2024-05-11 02:12:02 +0300

    add last version mb

--- main.c
+++ main.c
@@ -3,58 +3,174 @@
 #include <sys/inotify.h>
 #include <unistd.h>
 #include <string.h>
-#include <pthread.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <stdbool.h>
 
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define BUFFER_SIZE (1024 * (EVENT_SIZE + 16)) * 100
-#define MAX_THREADS 10
 
 typedef struct {
+    char root[512];
     char filename[256];
     char destination[256];
-} thread_data_t;
+} file_data_t;
 
-int active_threads = 0;
-pthread_mutex_t thread_lock = PTHREAD_MUTEX_INITIALIZER;
-
-void *copy_file(void *arg) {
-    thread_data_t *data = (thread_data_t *) arg;
+void copy_file(const file_data_t *data) {
     char command[512];
-    snprintf(command, sizeof(command), "rsync -avzhlHl --ignore-missing-args /home/egerin/OSISP/test/%s %s", data->filename, data->destination);
+    snprintf(command,
+             sizeof(command),
+             "rsync -avzhlHlr %s/%s %s",
+             data->root,
+             data->filename,
+             data->destination);
+
     system(command);
-    free(data);
-    pthread_mutex_lock(&thread_lock);
-    active_threads--;
-    pthread_mutex_unlock(&thread_lock);
-    return NULL;
 }
 
-void handle_event(const struct inotify_event *event, const char *destination_path) {
+char *memory(const int capacity) {
+    char *array = (char *) malloc(capacity * sizeof(char));
+    return array;
+}
+
+int is_valid_ip(const char *ip_adr) {
+    int dots = 0;
+    int len = (int) strlen(ip_adr);
+    if (len < 7 || len > 15)
+        return 0;
+    for (int i = 0; i < len; i++) {
+        if (!isdigit(ip_adr[i]) && ip_adr[i] != '.')
+            return 0;
+        if (ip_adr[i] == '.')
+            dots++;
+    }
+    if (dots != 3)
+        return 0;
+    char *token;
+    char *ptr;
+    char temp_ip[strlen(ip_adr) + 1];
+    strcpy(temp_ip, ip_adr);
+    token = strtok(temp_ip, ".");
+    while (token != NULL) {
+        long num = strtol(token, &ptr, 10);
+        if (num < 0 || num > 255)
+            return 0;
+        if (*ptr)
+            return 0;
+        token = strtok(NULL, ".");
+    }
+
+    return 1;
+}
+
+bool check_ip(char **string) {
+    if (strlen(*string) == 0) {
+        fprintf(stderr, "size < 0");
+        return false;
+    }
+
+    char *token = strtok(*string, "@");
+    if (token == NULL) {
+        fprintf(stderr, "Invalid format: missing '@'");
+        return false;
+    }
+
+    token = strtok(NULL, ":");
+    if (token == NULL) {
+        fprintf(stderr, "Invalid format: missing ':'");
+        return false;
+    }
+
+    if (!is_valid_ip(token)) {
+        return false;
+    }
+
+    return true;
+}
+
+int print_files(char *path) {
+    int count_file = 0;
+    DIR *dir;
+    struct dirent *entry;
+    char new_path[1024];
+
+    dir = opendir(path);
+    if (dir == NULL) {
+        printf("Ошибка открытия каталога: %s\n", path);
+        return 0;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        snprintf(new_path, sizeof(new_path), "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_REG) {
+            printf("Файл: %s\n", new_path);
+            count_file++;
+        }
+    }
+
+    closedir(dir);
+
+    return count_file;
+}
+
+void explore_directory(char *path, char **previous_path) {
+    DIR *dir;
+    struct dirent *entry;
+    char *new_path;
+
+    dir = opendir(path);
+    if (dir == NULL) {
+        printf("Ошибка открытия каталога: %s\n", path);
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        asprintf(&new_path, "%s/%s", path, entry->d_name);
+
+        if (entry->d_type == DT_DIR) {
+            if (strcmp(entry->d_name, "tmp") == 0) {
+                printf("Каталог: %s\n", new_path);
+                *previous_path = strdup(path);
+            } else {
+                printf("Каталог: %s\n", new_path);
+                int count_files_tmp = print_files(new_path);
+                printf("Всего файлов в каталоге %s: %d\n", new_path, count_files_tmp);
+                explore_directory(new_path, previous_path);
+            }
+        } else if (entry->d_type != DT_REG) {
+            printf("Неизвестный тип: %s\n", new_path);
+        }
+
+        free(new_path);
+    }
+
+    closedir(dir);
+}
+
+
+void handle_event(const struct inotify_event *event, const char *destination_path, const char *user_path) {
     printf("Event: %s\n", event->name);
+    file_data_t *data = malloc(sizeof(file_data_t));
+    if (data == NULL) {
+        perror("malloc");
+        return;
+    }
+    strcpy(data->root, user_path);
+    strcpy(data->destination, destination_path);
+    strcpy(data->filename, event->name);
 
     switch (event->mask) {
         case IN_CREATE:
         case IN_MOVED_TO:
             printf("File created: %s\n", event->name);
-            pthread_mutex_lock(&thread_lock);
-            if (active_threads < MAX_THREADS) {
-                active_threads++;
-                pthread_mutex_unlock(&thread_lock);
-                thread_data_t *data = malloc(sizeof(thread_data_t));
-                strcpy(data->filename, event->name);
-                strcpy(data->destination, destination_path);
-                pthread_t thread;
-                if (pthread_create(&thread, NULL, copy_file, data) != 0) {
-                    perror("pthread_create");
-                    free(data);
-                    pthread_mutex_lock(&thread_lock);
-                    active_threads--;
-                    pthread_mutex_unlock(&thread_lock);
-                }
-            } else {
-                pthread_mutex_unlock(&thread_lock);
-                printf("Maximum number of threads reached, skipping file: %s\n", event->name);
-            }
+            copy_file(data);
             break;
         case IN_DELETE:
             printf("File deleted: %s\n", event->name);
@@ -64,20 +180,35 @@ void handle_event(const struct inotify_event *event, const char *destination_pat
             break;
         default:
             printf("Unknown event: %s\n", event->name);
+            copy_file(data);
             break;
     }
+    free(data);
 }
 
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <destination path>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <destination path> <path to other >\n", argv[0]);
         return 1;
     }
 
-    char destination_path[256];
-    strcpy(destination_path, argv[1]);
+    char *destination_path = NULL;
+    char *root = NULL;
+    char *last_path = NULL;
+    asprintf(&destination_path, "%s", argv[1]);
+    asprintf(&root, "%s", argv[2]);
+    puts(destination_path);
+    puts(root);
+    explore_directory(root, &last_path);
+    puts(destination_path);
+    puts(root);
+    puts(last_path);
+    //    if (!check_ip(&root)) {
+    //        return 1;
+    //    }
 
-    int fd, wd;
+    int fd, wd, wd2;
     char buffer[BUFFER_SIZE];
 
     fd = inotify_init();
@@ -86,11 +217,30 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    wd = inotify_add_watch(fd, "/home/egerin/OSISP/test", IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_TO);    if (wd < 0) {
+    char *new_path = strdup(last_path);
+    strcat(new_path, "/tmp");
+    puts(new_path);
+    puts(last_path);
+
+    wd = inotify_add_watch(fd, new_path, IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_TO);
+
+    if (wd < 0) {
         perror("inotify_add_watch");
         close(fd);
+        free(new_path);
         return 1;
     }
+
+    wd2 = inotify_add_watch(fd, last_path, IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_TO);
+    if (wd2 < 0) {
+        perror("inotify_add_watch");
+        close(fd);
+        free(new_path);
+        return 1;
+    }
+
+
+    int max_files_in_last_path = print_files(last_path);
 
     while (1) {
         ssize_t len = read(fd, buffer, BUFFER_SIZE);
@@ -102,7 +252,20 @@ int main(int argc, char *argv[]) {
 
         for (char *p = buffer; p < buffer + len;) {
             struct inotify_event *event = (struct inotify_event *) p;
-            handle_event(event, destination_path);
+
+            if (event->wd == wd2) {
+                handle_event(event, destination_path, last_path);
+
+                if (event->mask == IN_CREATE || event->mask == IN_MOVED_TO) {
+                    max_files_in_last_path++;
+                    if (max_files_in_last_path >=1) {
+                        inotify_rm_watch(fd, wd);
+                    }
+                }
+            } else {
+                handle_event(event, destination_path, new_path);
+            }
+
             p += EVENT_SIZE + event->len;
         }
     }
@@ -110,3 +273,4 @@ int main(int argc, char *argv[]) {
     close(fd);
     return 0;
 }
+
